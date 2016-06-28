c8da19c26edcf5584c872dd410cc212cb70dee4b
Tobias <tobias@roomtailors.de>
2016-06-28 13:26:22 +0200

    Layout complete2

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,13 +27,55 @@ module.exports = function(grunt) {
                     ext: '.min.css'
                 }]
             }
-        }
+        },
+
+        // create images of different resolutions for responsive sourcing
+        responsive_images: {
+            img_multi_resolutions: {
+                options: {
+                    sizes: [{
+                        name: "small",
+                        width: 400
+                    }, {
+                        name: "medium",
+                        width: 800
+                    }, {
+                        name: "large",
+                        width: 1200
+                    }, {
+                        name: "xxl",
+                        width: 2000
+                    }]
+                },
+                files: [{
+                    expand: true,
+                    src: ['**/*.{jpg,gif,png}', '!**/responsive/**'],
+                    cwd: 'img-source/',
+                    dest: 'resources/img/responsive/'
+                }]
+            }
+        },
+
+        // minimize all images
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'resources/img/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'resources/img/'
+                }]
+            }
+        },
+
     });
 
     // Load Grunt Task Modules
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-responsive-images');
 
     // Define default tasks to run
-    grunt.registerTask('default', ['less', 'cssmin']);
+    grunt.registerTask('default', ['less', 'cssmin', 'responsive_images', 'imagemin']);
 };
